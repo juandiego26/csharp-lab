ac521d0aaa91f44866b78937d110b87cd7329e0e
Juan Diego <silgajuandiego@gmail.com>
2019-09-21 19:10:03 -0500

    [ADD] Add Entidades

--- etapa1/Entidades/Escuela.cs
+++ etapa1/Entidades/Escuela.cs
@@ -1,39 +1,42 @@
+using System;
 using System.Collections.Generic;
 
 namespace CoreEscuela.Entidades
 {
-    class Escuela
+  public class Escuela
+  {
+    public string UniqueId { get; private set; } = Guid.NewGuid().ToString();
+    string nombre;
+    public string Nombre
     {
-        string nombre;
-        public string Nombre {
-            get{ return nombre; }
-            set{ nombre = value.ToUpper(); }
-        }
-        public int AñoDeCreación { get; set; }
-        public string País { get; set; }
-        public string Ciudad { get; set; }
-        public TiposEscuela TipoEscuela { get; set; }
-        public List<Curso> Cursos { get; set; }
-        // public Escuela(string nombre, int año)
-        // {
-        //     this.nombre = nombre;
-        //     AñoDeCreación = año;
-        // }
-        public Escuela(string nombre, int año) => (Nombre, AñoDeCreación) = (nombre, año);
-        public Escuela(string nombre,
-                        int año,
-                        TiposEscuela tipo,
-                        string país = "",
-                        string ciudad = "")
-        {
-            (Nombre, AñoDeCreación) = (nombre, año);
-            País = país;
-            Ciudad = ciudad;
-        }
-
-        public override string ToString()
-        {
-            return $"Nombre: \"{Nombre}\",\nTipo: {TipoEscuela}, {System.Environment.NewLine}País: {País},\nCiudad: {Ciudad}";
-        }
+      get { return nombre; }
+      set { nombre = value.ToUpper(); }
     }
+    public int AñoDeCreación { get; set; }
+    public string País { get; set; }
+    public string Ciudad { get; set; }
+    public TiposEscuela TipoEscuela { get; set; }
+    public List<Curso> Cursos { get; set; }
+    // public Escuela(string nombre, int año)
+    // {
+    //     this.nombre = nombre;
+    //     AñoDeCreación = año;
+    // }
+    public Escuela(string nombre, int año) => (Nombre, AñoDeCreación) = (nombre, año);
+    public Escuela(string nombre,
+                    int año,
+                    TiposEscuela tipo,
+                    string país = "",
+                    string ciudad = "")
+    {
+      (Nombre, AñoDeCreación) = (nombre, año);
+      País = país;
+      Ciudad = ciudad;
+    }
+
+    public override string ToString()
+    {
+      return $"Nombre: \"{Nombre}\",\nTipo: {TipoEscuela}, {System.Environment.NewLine}País: {País},\nCiudad: {Ciudad}";
+    }
+  }
 }
--- etapa1/Program.cs
+++ etapa1/Program.cs
@@ -1,106 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using CoreEscuela.Entidades;
+using CoreEscuela.Util;
 using static System.Console;
 
-namespace etapa1
+namespace CoreEscuela
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria,
-                                        país: "Colombia", ciudad: "Bogotá");
+      var engine = new EscuelaEngine();
+      engine.Inicializar();
 
-            escuela.Cursos = new List<Curso>(){
-                new Curso() { Nombre = "101", Jornada = TiposJornada.Mañana },
-                new Curso() { Nombre = "201", Jornada = TiposJornada.Mañana },
-                new Curso() { Nombre = "301", Jornada = TiposJornada.Mañana }
-            };
+      Printer.WriteTitle("BIENVENIDOS A LA ESCUELA");
+      // Printer.Beep();
 
-            // adicionamos a la coleccion
-            escuela.Cursos.Add( new Curso {Nombre = "102", Jornada = TiposJornada.Tarde} );
-            escuela.Cursos.Add( new Curso {Nombre = "202", Jornada = TiposJornada.Tarde} );
-
-            var otraColección = new List<Curso>(){
-                new Curso() { Nombre = "401", Jornada = TiposJornada.Mañana },
-                new Curso() { Nombre = "501", Jornada = TiposJornada.Mañana },
-                new Curso() { Nombre = "501", Jornada = TiposJornada.Tarde }
-            };
-            escuela.Cursos.AddRange(otraColección);
-            imprimirCursosEscuela(escuela);
-
-            escuela.Cursos.RemoveAll(delegate(Curso cur)
-            {
-                return cur.Nombre == "301";
-            });
-
-            escuela.Cursos.RemoveAll((cur) => cur.Nombre == "501" && cur.Jornada == TiposJornada.Mañana);
-
-            WriteLine("======================");
-            // quitar todos miembros de una colección
-            // otraColección.Clear();
-
-// Como un Arreglo
-            // escuela.Cursos = new Curso[] {
-            //     new Curso() { Nombre = "101" },
-            //     new Curso() { Nombre = "201" },
-            //     new Curso() { Nombre = "301" }
-            // };
-            imprimirCursosEscuela(escuela);
-        }
-
-        // private static bool Predicado(Curso cursoobj)
-        // {
-        //     return cursoobj.Nombre == "301";
-        // }
-
-        private static void imprimirCursosEscuela(Escuela escuela)
-        {
-            WriteLine("===========================");
-            WriteLine("Cursos de la Escuela");
-            WriteLine("===========================");
-
-            if (escuela?.Cursos != null)
-            {
-                foreach (var curso in escuela.Cursos)
-                {
-                    WriteLine($"Nombre {curso.Nombre}, Id {curso.UniqueId}");
-                }
-            }
-        }
-        private static void ImprimirCursosWhile(Curso[] arregloCursos)
-        {
-            int contador = 0;
-            while (contador < arregloCursos.Length)
-            {
-                Console.WriteLine($"Nombre {arregloCursos[contador].Nombre}, Id {arregloCursos[contador].UniqueId}");
-                contador++;
-            }
-        }
-        private static void ImprimirCursosDoWhile(Curso[] arregloCursos)
-        {
-            int contador = 0;
-            do
-            {
-                Console.WriteLine($"Nombre {arregloCursos[contador].Nombre}, Id {arregloCursos[contador].UniqueId}");
-                contador++;
-            }while (contador < arregloCursos.Length);
-        }
-        private static void ImprimirCursosFor(Curso[] arregloCursos)
-        {
-            for (int i = 0; i < arregloCursos.Length; i++)
-            {
-                Console.WriteLine($"Nombre {arregloCursos[i].Nombre}, Id {arregloCursos[i].UniqueId}");
-            }
-        }
-        private static void ImprimirCursosForEach(Curso[] arregloCursos)
-        {
-            foreach (var curso in arregloCursos)
-            {
-                Console.WriteLine($"Nombre {curso.Nombre}, Id {curso.UniqueId}");
-
-            }
-        }
+      imprimirCursosEscuela(engine.Escuela);
     }
+    private static void imprimirCursosEscuela(Escuela escuela)
+    {
+      Printer.WriteTitle("Cursos de la Escuela");
+
+      if (escuela?.Cursos != null)
+      {
+        foreach (var curso in escuela.Cursos)
+        {
+          WriteLine($"Nombre {curso.Nombre}, Id {curso.UniqueId}");
+        }
+      }
+    }
+  }
 }
